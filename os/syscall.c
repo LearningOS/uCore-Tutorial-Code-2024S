75af1ae0b9bfef5184432ea70a42a81a4746c03d
rcy17 <rcycyzh@163.com>
2023-03-03 00:11:54 +0800

    Chapter 3

--- os/syscall.c
+++ os/syscall.c
@@ -2,6 +2,7 @@
 #include "defs.h"
 #include "loader.h"
 #include "syscall_ids.h"
+#include "timer.h"
 #include "trap.h"
 
 uint64 sys_write(int fd, char *str, uint len)
@@ -17,23 +18,41 @@ uint64 sys_write(int fd, char *str, uint len)
 
 __attribute__((noreturn)) void sys_exit(int code)
 {
-	debugf("sysexit(%d)", code);
-	run_next_app();
-	printf("ALL DONE\n");
-	shutdown();
+	exit(code);
 	__builtin_unreachable();
 }
+
+uint64 sys_sched_yield()
+{
+	yield();
+	return 0;
+}
+
+uint64 sys_gettimeofday(TimeVal *val, int _tz)
+{
+	uint64 cycle = get_cycle();
+	val->sec = cycle / CPU_FREQ;
+	val->usec = (cycle % CPU_FREQ) * 1000000 / CPU_FREQ;
+	return 0;
+}
+
+/*
+* LAB1: you may need to define sys_task_info here
+*/
 
 extern char trap_page[];
 
 void syscall()
 {
-	struct trapframe *trapframe = (struct trapframe *)trap_page;
+	struct trapframe *trapframe = curr_proc()->trapframe;
 	int id = trapframe->a7, ret;
 	uint64 args[6] = { trapframe->a0, trapframe->a1, trapframe->a2,
 			   trapframe->a3, trapframe->a4, trapframe->a5 };
 	tracef("syscall %d args = [%x, %x, %x, %x, %x, %x]", id, args[0],
 	       args[1], args[2], args[3], args[4], args[5]);
+	/*
+	* LAB1: you may need to update syscall counter for task info here
+	*/
 	switch (id) {
 	case SYS_write:
 		ret = sys_write(args[0], (char *)args[1], args[2]);
@@ -41,6 +60,15 @@ void syscall()
 	case SYS_exit:
 		sys_exit(args[0]);
 		// __builtin_unreachable();
+	case SYS_sched_yield:
+		ret = sys_sched_yield();
+		break;
+	case SYS_gettimeofday:
+		ret = sys_gettimeofday((TimeVal *)args[0], args[1]);
+		break;
+	/*
+	* LAB1: you may need to add SYS_taskinfo case here
+	*/
 	default:
 		ret = -1;
 		errorf("unknown syscall %d", id);
